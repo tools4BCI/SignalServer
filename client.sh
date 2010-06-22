0fbd76188cd1a740ec614b9bd94be9b3f339ffd5
Andreas <andreas@localhost.localdomain>
2010-06-22 10:27:24 +0200

    adding startup script for signalserver-sclient

---
+++ client.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+CLIENT_CMD="signalserver-sclient"
+
+if [ ! -f bin/${CLIENT_CMD} ]; then
+  echo "** ERROR - bin/${CLIENT_CMD} does not exist."
+  exit 1
+fi
+
+exec env LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/lib bin/${CLIENT_CMD}
