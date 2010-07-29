7df5fb9e2ff6c46a265ad840c4946aab07c8e2d4
Andreas <andreas@localhost.localdomain>
2010-07-29 08:49:56 +0200

    updated doxygen

--- include/signalserver-client/ssclientimpl.h
+++ include/signalserver-client/ssclientimpl.h
@@ -1,7 +1,7 @@
 /**
 * @file server_server.h
 *
-* @brief
+* @brief Declaration of the base implementation of the SSClientImplBase interface
 *
 **/
 
@@ -29,86 +29,91 @@ class SSConfig;
 
 //-----------------------------------------------------------------------------
 /**
-* @class SSConfig
+* @class SSClientImpl
 *
-* @brief The core of the Signal Server
+* @brief Base implementation of the SSClient
 *
-* @todo
 */
 class SSClientImpl : public SSClientImplBase
 {
 public:
   /**
-  * @brief Destructor
-  * @param[in]  packet
-  * @throws
+  * @brief Default Constructor
   */
   SSClientImpl();
   /**
   * @brief Destructor
-  * @param[in]  packet
-  * @throws
   */
   virtual ~SSClientImpl();
   /**
-  * @brief Destructor
-  * @param[in]  packet
-  * @throws
+  *  @brief Establish a connection to a TOBI SignalServer
+  * The caller will be blocked until the connection has been made or an error has occurred.
+  * @param address host name or the IPv4 address of the server
+  * @param port the control connection port the server listens on
+  * @throw std::ios_base::failure if the client could not
+  *   connect to the server or if the client is already connected.
   */
   virtual void connect(const std::string& address, short unsigned port);
   /**
-  * @brief Destructor
-  * @param[in]  packet
-  * @throws
-  */
+   * @brief Tells if the client is connected to the server,
+   *        i.e. if the control connection has been established.
+   * @return \c true if connected \c false otherwise.
+   * @sa connect()
+   */
   virtual bool connected() const;
   /**
-  * @brief Destructor
-  * @param[in]  packet
-  * @throws
-  */
+   * @brief Disconnects from the server, i.e. stops receiving and closes the control connection.
+   * @throw std::ios_base::failure if an occurs while disconnecting
+   * \sa connect(), connected(), receiving()
+   */
   virtual void disconnect();
   /**
-  * @brief Destructor
-  * @param[in]  packet
-  * @throws
-  */
+   * @brief Requests the meta data information from the server
+   * The caller will be blocked until the request has been processed or an error has occurred.
+   * @throw std::ios_base::failure if the client is not connected or if an error occurred
+   * sa config()
+   */
   virtual void requestConfig();
   /**
-  * @brief Destructor
-  * @param[in]  packet
-  * @throws
-  */
+   * @brief Returns the meta data information requested from the server
+   * @sa requestConfig()
+   */
   virtual SSConfig config() const;
   /**
-  * @brief Destructor
-  * @param[in]  packet
-  * @throws
-  */
+   * @brief Turns the client into receiving state
+   * The caller will be blocked until the client is ready to receive or an error has occurred.
+   * @param use_udp_bc if \c true data will be received via UDP broadcast instead of TCP
+   * @throw std::ios_base::failure if the client is not connected or if an error occurred
+   * \sa stopReceiving(), receiving(), getDataPacket()
+   */
   virtual void startReceiving(bool use_udp_bc);
   /**
-  * @brief Destructor
-  * @param[in]  packet
-  * @throws
-  */
+   * @brief Tell if the client if is in receiving state
+   * @return \c true if the client is in receiving state, \c false otherwise.
+   * \sa startReceiving(), stopReceiving()
+   */
   virtual bool receiving() const;
   /**
-  * @brief Destructor
-  * @param[in]  packet
-  * @throws
-  */
+   * @brief Stops receiving, i.e. calling getDataPacket() will fail afterwards.
+   * @throw std::ios_base::failure if the client is not connected or if an error occurred
+   * \sa startReceiving(), receiving()
+   */
   virtual void stopReceiving();
   /**
-  * @brief Destructor
-  * @param[in]  packet
-  * @throws
-  */
+   * @brief Gets a packet from the server.
+   * The caller will be blocked until a packet has been received or an error has occurred.
+   * @param[out] packet the received data packet
+   * @throw std::ios_base::failure if the client is not connected or isn't in receiving state or
+   *                               if a receiving error has occurred
+   * @throw std::overflow_error if an overflow occurs
+   *
+   */
   virtual void getDataPacket(DataPacket& packet);
+
   /**
-  * @brief Destructor
-  * @param[in]  packet
-  * @throws
-  */
+   * @brief Sets the client's data input buffer size to the given value
+   * @param size the size of the input buffer in [byte]
+   */
   virtual void setBufferSize(size_t size);
 
 protected:
@@ -127,15 +132,14 @@ protected:
 
 protected:
   /**
-  * @brief Destructor
-  * @param[in]  packet
-  * @throws
+  * @brief Establishes a data connection to the SignalServer
+  * @param use_udp_bc if \c true data will be received via UDP broadcast instead of TCP
+  * @throws std::ios_base::failure if an error has occurred
   */
   void establishDataConnection(bool use_udp_bc);
   /**
-  * @brief Destructor
-  * @param[in]  packet
-  * @throws
+  * @brief Closes the data connection
+  * @throws std::ios_base::failure if an error has occurred
   */
   void closeDataConnection();
 
