69eced2ae676cd3c5d559090b7bbef7b73ffd508
Bettina Kaltner <b.kaltner@student.tugraz.at>
2010-10-11 11:18:14 +0200

    mouse running async. on linux and windows

--- include/version.h
+++ include/version.h
@@ -1,6 +1,6 @@
 #ifndef _VERSION_H
   #define _VERSION_H
   #define MAJOR_VERSION "0.1"
-  #define BUILD_NUMBER 45
-  #define BUILD_STR " Fri Sep 24 11:10:58 CEST 2010"
+  #define BUILD_NUMBER 65
+  #define BUILD_STR " Mon Oct 11 11:07:33 CEST 2010"
 #endif   //_VERSION_H
