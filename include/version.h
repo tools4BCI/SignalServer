fe17b3cebee04dae2f46ee976866a8866ed6e89a
Bettina Kaltner <b.kaltner@student.tugraz.at>
2010-10-21 10:44:36 +0200

    cleared out libusb linux

--- include/version.h
+++ include/version.h
@@ -1,6 +1,6 @@
 #ifndef _VERSION_H
   #define _VERSION_H
   #define MAJOR_VERSION "0.1"
-  #define BUILD_NUMBER 65
-  #define BUILD_STR " Mon Oct 11 11:07:33 CEST 2010"
+  #define BUILD_NUMBER 89
+  #define BUILD_STR " Thu Oct 21 10:40:33 CEST 2010"
 #endif   //_VERSION_H
