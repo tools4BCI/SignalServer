47e2cc2ad201328cb0f5269f341c55b73c627759
Christoph Eibel <eibel@bcipc0xx.(none)>
2010-10-27 10:55:50 +0200

    JStick and Mouse (linux) are constructed via HWThreadFactory now

--- include/hardware/hw_thread_builder.h
+++ include/hardware/hw_thread_builder.h
@@ -98,6 +98,80 @@ private:
     }
 };
 
+//-----------------------------------------------------------------------------
+/**
+* @class HWThreadBuilderTemplateRegistratorWithoutIOService
+*
+* @brief A template for the HWThreadBuilder which also registrates concrete builders
+*        in the HWThreadFactory
+*
+* usage: create an instance of this class (static class member or a global variable)
+*        HWThreadBuilderTemplateRegistratorWithoutIOService<ConcreteHWThread> my_concrete_hw_thread_builder ("blub", "bla", ...);
+*/
+template<typename T>
+class HWThreadBuilderTemplateRegistratorWithoutIOService : public HWThreadBuilder
+{
+public:
+    /**
+      * as far as C++ doesn't support initializer lists, for any number of
+      * keys a constructor is necessary
+      */
+    HWThreadBuilderTemplateRegistratorWithoutIOService (std::string key)
+    {
+        registerKey (key);
+    }
+
+    HWThreadBuilderTemplateRegistratorWithoutIOService (std::string key_1, std::string key_2)
+    {
+        registerKey (key_1);
+        registerKey (key_2);
+    }
+    HWThreadBuilderTemplateRegistratorWithoutIOService (std::string key_1, std::string key_2, std::string key_3)
+    {
+        registerKey (key_1);
+        registerKey (key_2);
+        registerKey (key_3);
+    }
+    HWThreadBuilderTemplateRegistratorWithoutIOService (std::string key_1, std::string key_2, std::string key_3, std::string key_4)
+    {
+        registerKey (key_1);
+        registerKey (key_2);
+        registerKey (key_3);
+        registerKey (key_4);
+    }
+    HWThreadBuilderTemplateRegistratorWithoutIOService (std::string key_1, std::string key_2, std::string key_3, std::string key_4, std::string key_5)
+    {
+        registerKey (key_1);
+        registerKey (key_2);
+        registerKey (key_3);
+        registerKey (key_4);
+        registerKey (key_5);
+    }
+    HWThreadBuilderTemplateRegistratorWithoutIOService (std::string key_1, std::string key_2, std::string key_3, std::string key_4, std::string key_5, std::string key_6)
+    {
+        registerKey (key_1);
+        registerKey (key_2);
+        registerKey (key_3);
+        registerKey (key_4);
+        registerKey (key_5);
+        registerKey (key_6);
+    }
+
+    virtual HWThread* createInstance (boost::asio::io_service& io, XMLParser& parser, ticpp::Iterator<ticpp::Element> hw) const
+    {
+        return new T (parser, hw);
+    }
+
+private:
+    HWThreadBuilderTemplateRegistratorWithoutIOService () {}
+
+    void registerKey (std::string const& key)
+    {
+        HWThreadFactory::instance().registerBuilder (key, new HWThreadBuilderTemplateRegistratorWithoutIOService<T>);
+    }
+};
+
+
 
 } // namespace tobiss
 
