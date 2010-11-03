c02e016f4a52e665021dbec9661181407a0fbc22
Christian Breitwieser <c.breitwieser@tugraz.at>
2010-11-03 12:02:58 +0100

    Added gpl and lgpl licence preambles to all files; included licences.

--- include/version.h
+++ include/version.h
@@ -1,6 +1,6 @@
 #ifndef _VERSION_H
   #define _VERSION_H
   #define MAJOR_VERSION "0.1"
-  #define BUILD_NUMBER 98
-  #define BUILD_STR " Thu Oct 21 12:50:27 CEST 2010"
+  #define BUILD_NUMBER 107
+  #define BUILD_STR " Mit Nov  3 12:02:45 CET 2010"
 #endif   //_VERSION_H
