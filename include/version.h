79e2bedc37f1e208fd564aeebe862c0c328105e7
Christian Breitwieser <c.breitwieser@tugraz.at>
2010-11-18 15:20:53 +0100

    New major version 0.2

--- include/version.h
+++ include/version.h
@@ -1,6 +1,6 @@
 #ifndef _VERSION_H
   #define _VERSION_H
   #define MAJOR_VERSION "0.1"
-  #define BUILD_NUMBER 176
-  #define BUILD_STR " Don Nov 18 09:11:30 CET 2010"
+  #define BUILD_NUMBER  1
+  #define BUILD_STR " Don Nov 18 10:09:18 CET 2010"
 #endif   //_VERSION_H
