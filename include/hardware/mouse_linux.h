21d87683a53f75a193c1c7d63ae397eb892dcf5b
Bettina Kaltner <b.kaltner@student.tugraz.at>
2010-11-19 16:07:32 +0100

    Switch operating system

--- include/hardware/mouse_linux.h
+++ include/hardware/mouse_linux.h
@@ -19,14 +19,16 @@
 */
 
 /**
-* @file mouse.h
+* @file mouse_linux.h
 *
 * @brief
 *
 **/
 
-#ifndef MOUSE_H
-#define MOUSE_H
+
+
+#ifndef MOUSE_LINUX_H
+#define MOUSE_LINUX_H
 
 #include <boost/cstdint.hpp>
 #include <set>
@@ -34,93 +36,43 @@
 #include <usb.h>
 
 #include "hw_thread.h"
+#include "mouse.h"
 #include "hw_thread_builder.h"
-#include "extern/lib/libusb/linux/libusb-1.0.8/libusb/libusb.h"
+#include "extern/include/libusb/libusb.h"
 
 namespace tobiss
 {
-//-----------------------------------------------------------------------------
 
-class Mouse : public HWThread
+    class Mouse : public MouseBase
     {
     public:
-      Mouse(ticpp::Iterator<ticpp::Element> hw);
 
-      /**
-      * @brief Destructor
-      */
-      virtual ~Mouse();
+        Mouse(ticpp::Iterator<ticpp::Element> hw);
 
-      /**
-      * @brief Method to achieve asynchronous data acquisition (method is non-blocking).
-      * @return SampleBlock<double>
-      *
-      * This method return immediately after calling with a copy of the data stored in the device's
-      * buffer.
-      * It is called from all slave devices.
-      */
-      virtual SampleBlock<double> getAsyncData();
-      /**
-      * @brief Method to start data acquisition.
-      */
-      virtual void run();
-      /**
-      * @brief Method to stop data acquisition.
-      */
-      virtual void stop();
+        /**
+        * @brief Destructor
+        */
+//        virtual ~MouseBase();
 
-      //-----------------------------------------------
-    private:
+        /**
+        * @brief Method to start data acquisition.
+        */
+        virtual void run();
 
-  //     void setHardware(ticpp::Iterator<ticpp::Element>const &hw);
+    protected:
+        virtual int blockKernelDriver();
+        virtual int freeKernelDriver();
+        virtual void acquireData();
 
-      virtual void setDeviceSettings(ticpp::Iterator<ticpp::Element>const &father);
-
-      virtual void setChannelSettings(ticpp::Iterator<ticpp::Element>const &father);
-
-      virtual SampleBlock<double> getSyncData()   {return data_; }
-
-      void initMouse();
-      int blockKernelDriver();
-      int freeKernelDriver();
-      void acquireData();
-
-      //-----------------------------------------------
-
-    private:
-      static std::set<boost::uint16_t> used_ids_;
-
-      boost::uint16_t id_;
-
-      boost::uint16_t buttons_;
-      std::vector<bool> buttons_values_;
-
-      boost::uint16_t axes_;
-      std::vector<boost::int16_t> axes_values_;
-
-      std::string name_;
-
-      bool user_interrupt_;
-
-      SampleBlock<double> empty_block_;
-
-      int x_,y_;
-      libusb_device **devs_; //pointer to pointer of device, used to retrieve a list of devices
-      libusb_device_handle *dev_handle_; //a device handle
-      libusb_context *ctx_; //a libusb session
-
-      boost::thread*  async_acqu_thread_;
-      std::vector<boost::int16_t>  raw_data_;
-      unsigned char async_data_[10];
-      bool dirty_;
-
-      static const HWThreadBuilderTemplateRegistratorWithoutIOService<Mouse> FACTORY_REGISTRATOR_;
-  };
+        libusb_device **devs_; //pointer to pointer of device, used to retrieve a list of devices
+        libusb_device_handle *dev_handle_; //a device handle
+        libusb_context *ctx_; //a libusb session
 
 
-
-} // Namespace tobiss
-
-#endif // MOUSE_H
+        static const HWThreadBuilderTemplateRegistratorWithoutIOService<Mouse> FACTORY_REGISTRATOR_;
+    };
 
 //-----------------------------------------------------------------------------
+
+}
+#endif // MOUSE_LINUX_H
--- include/version.h
+++ include/version.h
@@ -1,6 +1,6 @@
 #ifndef _VERSION_H
   #define _VERSION_H
   #define MAJOR_VERSION "0.1"
-  #define BUILD_NUMBER 169
-  #define BUILD_STR " Tue Nov  9 11:46:56 CET 2010"
+  #define BUILD_NUMBER 301
+  #define BUILD_STR " Fri Nov 19 16:04:54 CET 2010"
 #endif   //_VERSION_H
