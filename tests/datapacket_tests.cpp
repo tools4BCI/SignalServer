08977604d2761826c7a6bc6e4f0c7c48422eb1ba
Christoph Eibel <eibel@bcipc0xx.(none)>
2010-12-09 12:35:18 +0100

    datapacket3 tests

--- tests/datapacket_tests.cpp
+++ tests/datapacket_tests.cpp
@@ -17,7 +17,7 @@ using std::vector;
 ///
 /// emptyDataPacket
 ///
-/// checks if an newly created empty data packet is really empty
+/// checks if an newly created empty data packet behaves like an empty one
 ///
 TEST_FIXTURE(DataPacketSignalTypeFlags, emptyDataPacket)
 {
@@ -40,15 +40,22 @@ TEST_FIXTURE(DataPacketSignalTypeFlags, emptyDataPacket)
 }
 
 //-------------------------------------------------------------------------------------------------
-TEST_FIXTURE(DataPacketSetterGetterFixture, setterGetterDataPacket)
+///
+/// setterGetterDataPacket
+///
+/// check the setter and getter methods of DataPacket
+TEST(setterGetterDataPacket)
 {
     DataPacket3 empty_packet;
 
-    empty_packet.setConnectionPacketNr (packet_number);
-    CHECK(empty_packet.getConnectionPacketNr() == packet_number);
+    empty_packet.setConnectionPacketNr (5);
+    CHECK(empty_packet.getConnectionPacketNr() == 5);
 
-    empty_packet.setConnectionPacketNr (packet_number);
-    CHECK(empty_packet.getConnectionPacketNr() == packet_number);
+    empty_packet.setPacketID (10);
+    CHECK (empty_packet.getPacketID() == 10);
+
+    empty_packet.setTimestamp ();
+    CHECK_CLOSE (empty_packet.getTimestamp());
 }
 
 //-------------------------------------------------------------------------------------------------
@@ -74,9 +81,26 @@ TEST_FIXTURE(DataPacketRawMemoryFixture, createVersion3DataPacket)
     CHECK_EQUAL (empty_packet.getConnectionPacketNr(), RANDOM_CONNECTION_PACKET_NUMBER);
 
     DataPacket3 eeg_packet (reinterpret_cast<void*>(version_3_binary_packet_eeg));
-    //CHECK (empty_packet.getPacketNr() == );
+    CHECK_EQUAL (eeg_packet.getPacketID(), RANDOM_PACKET_ID);
+    CHECK_EQUAL (eeg_packet.getConnectionPacketNr(), RANDOM_CONNECTION_PACKET_NUMBER);
+    CHECK_EQUAL (eeg_packet.getNrOfChannels (SIG_EEG), EEG_CHANNELS);
+    BOOST_FOREACH (boost::uint32_t flag_setting, all_signal_type_flags_single)
+    {
+        if (flag_setting == SIG_EEG)
+            CHECK (eeg_packet.hasFlag (flag_setting));
+        else
+            CHECK (!eeg_packet.hasFlag (flag_setting));
+    }
+
 
     DataPacket3 eeg_emg_packet (reinterpret_cast<void*>(version_3_binary_packet_eeg_emg));
+    BOOST_FOREACH (boost::uint32_t flag_setting, all_signal_type_flags_single)
+    {
+        if (flag_setting == SIG_EEG || flag_setting == SIG_EMG)
+            CHECK (eeg_emg_packet.hasFlag (flag_setting));
+        else
+            CHECK (!eeg_emg_packet.hasFlag (flag_setting));
+    }
 }
 
 //-------------------------------------------------------------------------------------------------
