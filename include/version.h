f01ac8ab9c33cafd14bafd681e05bcd51ba47674
Bettina Kaltner <b.kaltner@student.tugraz.at>
2010-12-21 12:09:28 +0100

    prepare merge with master

--- include/version.h
+++ include/version.h
@@ -1,6 +1,6 @@
 #ifndef _VERSION_H
   #define _VERSION_H
-  #define MAJOR_VERSION "0.1"
-  #define BUILD_NUMBER  1
-  #define BUILD_STR " Don Nov 18 10:09:18 CET 2010"
+  #define MAJOR_VERSION "0.2"
+  #define BUILD_NUMBER 2
+  #define BUILD_STR " Mon Nov 22 13:21:50 CET 2010"
 #endif   //_VERSION_H
