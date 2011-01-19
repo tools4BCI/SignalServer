e742151a6d1b4dadf73804c7b2cd560673e9bfe5
Christoph Eibel <eibel@bcipc0xx.(none)>
2011-01-19 14:13:40 +0100

    implemented FustyDataServerImpl

---
+++ src/tia/server/fusty_data_server_impl.cpp
@@ -0,0 +1,111 @@
+#include "tia-private/server/fusty_data_server_impl.h"
+#include "tia-private/server/tia_exceptions.h"
+
+namespace tia
+{
+
+//-----------------------------------------------------------------------------
+FustyDataServerImpl::FustyDataServerImpl (tobiss::TCPDataServer& tcp_data_server,
+                                          tobiss::UDPDataServer& udp_data_server)
+    : tcp_data_server_ (tcp_data_server),
+      udp_data_server_ (udp_data_server),
+      next_free_connection_id_ (0)
+{
+
+}
+
+//-----------------------------------------------------------------------------
+FustyDataServerImpl::~FustyDataServerImpl ()
+{
+
+}
+
+//-----------------------------------------------------------------------------
+Port FustyDataServerImpl::localPort (ConnectionID connection) const
+{
+    if (tcp_connections_.count (connection))
+        return id_tcp_endpoint_map_[connection].port ();
+    else
+        return udp_data_server_.destination ().port ();
+
+    throw TiAException ("FustyDataServerImpl::localPort: No connection with that id.");
+}
+
+//-----------------------------------------------------------------------------
+ConnectionID  FustyDataServerImpl::addConnection (bool udp)
+{
+    ConnectionID new_connection_id = next_free_connection_id_;
+    next_free_connection_id_++;
+
+    if (udp)
+    {
+        udp_data_server_.incClientCount ();
+        udp_connections_.insert (new_connection_id);
+    }
+    else
+    {
+        boost::asio::ip::tcp::endpoint tcp_endpoint = tcp_data_server_.addConnection ();
+        tcp_connections_.insert (new_connection_id);
+        id_tcp_endpoint_map_[new_connection_id] = tcp_endpoint;
+    }
+
+    return new_connection_id;
+}
+
+//-----------------------------------------------------------------------------
+bool FustyDataServerImpl::hasConnection (ConnectionID connection) const
+{
+    if (tcp_connections_.count (connection))
+        return true;
+    if (udp_connections_.count (connection))
+        return true;
+
+    return false;
+}
+
+//-----------------------------------------------------------------------------
+bool FustyDataServerImpl::transmitting (ConnectionID connection) const
+{
+    return transmitting_.count (connection);
+}
+
+//-----------------------------------------------------------------------------
+bool FustyDataServerImpl::removeConnection (ConnectionID connection)
+{
+    if (tcp_connections_.count (connection))
+    {
+        tcp_connections_.erase (connection);
+        tcp_data_server_.removeConnection (id_tcp_endpoint_map_[connection]);
+        id_tcp_endpoint_map_.erase (connection);
+        return true;
+    }
+    else if (udp_connections_.count (connection))
+    {
+        udp_connections_.erase (connection);
+        udp_data_server_.decClientCount ();
+        return true;
+    }
+    return false;
+}
+
+//-----------------------------------------------------------------------------
+void FustyDataServerImpl::startTransmission (ConnectionID connection)
+{
+    if (tcp_connections_.count (connection))
+        tcp_data_server_.enableTransmission (id_tcp_endpoint_map_[connection], true);
+
+    transmitting_.insert (connection);
+}
+
+//-----------------------------------------------------------------------------
+void FustyDataServerImpl::stopTransmission (ConnectionID connection)
+{
+    if (tcp_connections_.count (connection))
+        tcp_data_server_.enableTransmission (id_tcp_endpoint_map_[connection], false);
+
+    transmitting_.erase (connection);
+}
+
+
+
+}
