ed2f58f8c1f4f341d225a4dbe811df4cc63164a8
Christoph Eibel <eibel@bcipc0xx.(none)>
2011-01-25 10:20:59 +0100

    further implemented new tia client impl

---
+++ include/tia-private/server/tia_datapacket.h
@@ -0,0 +1,19 @@
+#ifndef TIA_DATAPACKET_H
+#define TIA_DATAPACKET_H
+
+namespace tia
+{
+
+//-----------------------------------------------------------------------------
+/// DataPacket access-class
+class TiADataPacket
+{
+public:
+    ~TiADataPacket ();
+
+
+};
+
+}
+
+#endif // TIA_DATAPACKET_H
