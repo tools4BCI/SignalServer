7ace9bcb79de0e06aecd330c3090de5cc031bd95
Mario Rowies <mario.rowies@student.tugraz.at>
2011-08-01 09:47:56 +0200

    funktioniert soweit, bis auf "LoadLibrary nidaqmx.lib"

--- include/hardware/nidaqmx_wrapper.h
+++ include/hardware/nidaqmx_wrapper.h
@@ -3852,9 +3852,9 @@ class NIDaqmxWrapper
   public:
   NIDaqmxWrapper () : dll_handle_ (NULL)
   {
-    dll_handle_ = LoadLibrary("XXYYZZ.dll");
+    dll_handle_ = LoadLibrary("NIDAQmx.lib");
     if (dll_handle_ == NULL)
-      throw std::runtime_error ("Can't load XXYYZZ.dll");
+      throw std::runtime_error ("Can't load NIDAQmx.lib");
 
     setupDLLFunction (ni_DAQmxStartTask_ptr_, "DAQmxStartTask");
     setupDLLFunction (ni_DAQmxStopTask_ptr_,  "DAQmxStopTask");
@@ -3931,7 +3931,7 @@ class NIDaqmxWrapper
   {
     pointer = (T)GetProcAddress (dll_handle_, name.c_str());
     if (pointer == NULL)
-      throw std::runtime_error (std::string ("XXYYZZ.dll: missing function ").append (name));
+      throw std::runtime_error (std::string ("NIDAQmx.lib: missing function ").append (name));
   }
 
   HINSTANCE dll_handle_;
